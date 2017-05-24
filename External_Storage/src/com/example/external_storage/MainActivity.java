810b64008bb2291a9b87bc063aa69f4ef6acaecd
TranThiSoan <tranthisoan1211@gmail.com>
2017-05-24 20:23:29 +0700

    xu li file

---
+++ External_Storage/src/com/example/external_storage/MainActivity.java
@@ -0,0 +1,87 @@
+package com.example.external_storage;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Scanner;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends Activity {
+
+	Button btnread,btnwrite;
+	EditText editdata;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btnread=(Button) findViewById(R.id.btnreaddata);
+        btnwrite=(Button) findViewById(R.id.btnwritedata);
+        editdata=(EditText) findViewById(R.id.editdata);
+       // btnread.setOnClickListener((OnClickListener) this);
+       // btnwrite.setOnClickListener((OnClickListener) this);
+    }
+    
+    
+    
+    public void docdulieu(View v) {
+    	readData();
+    }
+    public void ghidulieu(View v) {
+    	writeData();
+    }
+    
+    public void readData()
+    {
+    	String sdcard=Environment.getExternalStorageDirectory().getAbsolutePath()+"/myfile.txt";
+
+    	try {
+    		 Scanner scan=new Scanner(new File(sdcard));
+    		 String data="";
+    		 while(scan.hasNext())
+    		 {
+    		 data+=scan.nextLine()+"\n";
+    		 }
+    		 scan.close();
+    		 editdata.setText(data+"");
+    		 } 
+    	catch (FileNotFoundException e) {
+    		 e.printStackTrace();
+    		 }
+    }
+    public void writeData()
+    {
+	    String sdcard=Environment.getExternalStorageDirectory().getAbsolutePath()+"/myfile.txt";
+	    try {
+		    OutputStreamWriter writer=
+		    new OutputStreamWriter(
+		    new FileOutputStream(sdcard));
+		    writer.write(editdata.getText()+"");
+		    writer.close();
+	    } 
+	    catch (FileNotFoundException e) {
+	    	e.printStackTrace();
+	    }
+	    catch (IOException e) {
+	    	e.printStackTrace();
+	    }
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+}
